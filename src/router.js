f5e074157a509be3e22183dac4ee4a80c8d43874
KER <ker26772@gmail.com>
2024-01-19 10:06:01 +0900

    20240119-2

--- src/router.js
+++ src/router.js
@@ -1,38 +1,37 @@
 import { Outlet, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Banana from "./router/Banana";
-import Grape from "./router/Grape";
+import TV from "./router/TV";
 import NotFound from "./router/NotFound";
 import Detail from "./router/Detail";
 
 import Search from "./router/Search";
-
+import Chakra from "./router/Chakra";
 
 const router = createBrowserRouter([
   {
     path: "",
-    element: <Outlet/>,
+    element: <Outlet />,
     errorElement: <NotFound />,
-    children: [
-      {path: "/", element: <App />},
-    ]
+    children: [{ path: "/", element: <App /> }],
   },
   {
     path: "/banana",
     element: <Banana />,
   },
   {
-    path: "/grape",
-    element: <Grape />,
+    path: "/tv",
+    element: <TV />,
   },
   {
-    path:"/detail/:id",
-    element: <Detail />
+    path: "/detail/:id",
+    element: <Detail />,
   },
   {
-    path:"/search/",
-    element:<Search/>  }
-
+    path: "/search/",
+    element: <Search />,
+  },
+  { path: "/chakra", element: <Chakra /> },
 ]);
 
 export default router;
--- src/router/Grape.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Grape() {
-  return (
-    <div>Grape</div>
-  )
-}
